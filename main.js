eb7e3c10e91d225b3528baa6fab4ebbe180c92b3
shohzod3112 <itsmygml6@gmail.com>
2024-08-02 15:49:18 +0500

    first commit

---
+++ main.js
@@ -0,0 +1,8 @@
+// Select the <select> element
+const selectElement = document.querySelector('#mySelect');
+
+// Get the value of the selected option
+const selectedValue = selectElement.value;
+
+// Log the value to the console
+console.log(selectedValue);
